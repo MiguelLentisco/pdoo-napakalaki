82fa7914f7ee107df1b0b4c2876aa893f3895264
José Antonio Álvarez <ocetebt@hotmail.com>
2017-05-26 12:19:27 +0200

    Miniupdate

--- Java_version/Napakalaki/src/napakalaki/Dudas.java
+++ Java_version/Napakalaki/src/napakalaki/Dudas.java
@@ -4,6 +4,8 @@ napakalakiView::currentPlayerViewKeyPressed sobra (lo añadi por error y no se q
 Ponerlo todo bonito, incluyendo monstruo, tesoros y pending bad consequnece
 el Text del pending BC destroza NapakalakiView
 
+NUEVO METODO PARA EL PENDING BADSHIT, PARA MOSTRAR SOLO LO QUE TE TIENES QUE QUITAR
+
     ERRORES:
 discardAllTreasures tiene que funcionar
 Player::MakeTreasureVisible no funciona bien (he equipado a la vez un tesoro de una mano y uno de dos manos)
